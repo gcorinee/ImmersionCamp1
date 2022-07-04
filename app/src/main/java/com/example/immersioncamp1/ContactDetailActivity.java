cd2b9d963b8f81708357e96533c397c59ddedfec
wooil <wooilkim9@gmail.com>
2022-07-04 20:24:38 +0900

    enable to edit contact

--- app/src/main/java/com/example/immersioncamp1/ContactDetailActivity.java
+++ app/src/main/java/com/example/immersioncamp1/ContactDetailActivity.java
@@ -1,6 +1,7 @@
 package com.example.immersioncamp1;
 
 import android.Manifest;
+import android.content.ContentProviderOperation;
 import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
@@ -13,7 +14,11 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -33,6 +38,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 public class ContactDetailActivity extends AppCompatActivity {
@@ -42,7 +49,7 @@ public class ContactDetailActivity extends AppCompatActivity {
     // creating variables for our image view and text view and string.
     private ContactsModal modal;
     private String contactId, name, phoneNumber, organization, email;
-    private TextView phoneTV, nameTV, organizationTV, emailTV;
+    private EditText nameET, phoneET, emailET, organizationET;
     private ImageView contactIV;
     private LinearLayout topBar;
     private CardView callCV, messageCV;
@@ -61,11 +68,11 @@ public class ContactDetailActivity extends AppCompatActivity {
         Log.e(null, "modal: " + modal.getUserName() + "  and  " + modal.getContactId());
         contactId = modal.getContactId();
         // initializing our views.
-        nameTV = findViewById(R.id.idTVName);
+        nameET = findViewById(R.id.idETName);
         contactIV = findViewById(R.id.idIVContact);
-        phoneTV = findViewById(R.id.idTVPhone);
-        organizationTV = findViewById(R.id.idTVOrganization);
-        emailTV = findViewById(R.id.idTVEmail);
+        phoneET = findViewById(R.id.idETPhone);
+        organizationET = findViewById(R.id.idETOrganization);
+        emailET = findViewById(R.id.idETEmail);
         callCV = findViewById(R.id.idCVCall);
         messageCV = findViewById(R.id.idCVMessage);
         topBar = findViewById(R.id.topBar);
@@ -105,6 +112,66 @@ public class ContactDetailActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), CustomGalleryActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        phoneET.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_DONE) {
+                    updatePhone(phoneET.getText().toString());
+                    setView();
+                    Toast.makeText(ContactDetailActivity.this, "번호 수정 성공", Toast.LENGTH_SHORT).show();
+                    phoneET.clearFocus();
+                    hideKeyboard(phoneET);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        organizationET.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_DONE) {
+                    updateOrganization(organizationET.getText().toString());
+                    setView();
+                    Toast.makeText(ContactDetailActivity.this, "직업 수정 성공", Toast.LENGTH_SHORT).show();
+                    organizationET.clearFocus();
+                    hideKeyboard(organizationET);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        nameET.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_DONE) {
+                    updateName(nameET.getText().toString());
+                    setView();
+                    Toast.makeText(ContactDetailActivity.this, "이름 수정 성공", Toast.LENGTH_SHORT).show();
+                    nameET.clearFocus();
+                    hideKeyboard(nameET);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        emailET.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_DONE) {
+                    updateEmail(emailET.getText().toString());
+                    setView();
+                    Toast.makeText(ContactDetailActivity.this, "이메일 수정 성공", Toast.LENGTH_SHORT).show();
+                    emailET.clearFocus();
+                    hideKeyboard(emailET);
+                    return true;
+                }
+                return false;
             }
         });
     }
@@ -190,10 +257,10 @@ public class ContactDetailActivity extends AppCompatActivity {
         email = modal.getEmail();
 
 
-        nameTV.setText(name);
-        phoneTV.setText(phoneNumber);
-        organizationTV.setText(organization);
-        emailTV.setText(email);
+        nameET.setText(name);
+        phoneET.setText(phoneNumber);
+        organizationET.setText(organization);
+        emailET.setText(email);
         contactIV.setClipToOutline(true);
         Bitmap bitmap = MainActivity.getPhoto(this, modal.getPhotoUri());
         Log.e(null, "bitmap get");
@@ -234,5 +301,69 @@ public class ContactDetailActivity extends AppCompatActivity {
             }
             Log.e(null, "55glmgl");
         }
+    }
+
+    private void updatePhone (String newPhone) {
+        String rawContactId = String.valueOf(MainActivity.getRawContactId(context, contactId));
+        ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
+        String selectPhone = ContactsContract.Data.CONTACT_ID + "=? AND " + ContactsContract.Data.MIMETYPE + "='"  +
+                ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE + "'" + " AND " + ContactsContract.CommonDataKinds.Phone.TYPE + "=?";
+        String[] phoneArgs = new String[]{rawContactId, String.valueOf(ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)};
+        ops.add(ContentProviderOperation.newUpdate(ContactsContract.Data.CONTENT_URI)
+                .withSelection(selectPhone, phoneArgs)
+                .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, newPhone)
+                .build());
+        try { getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops); } catch (Exception e) { Log.e(null, e.toString()); }
+        modal.setPhoneNumber(newPhone);
+        Log.e(null, "SUCCEED with " + newPhone);
+    }
+
+    private void updateOrganization (String newOrganization) {
+        String rawContactId = String.valueOf(MainActivity.getRawContactId(context, contactId));
+        ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
+        String selection = ContactsContract.Data.RAW_CONTACT_ID + "=? AND " + ContactsContract.Data.MIMETYPE + "=?";
+        String[] selectionArgs = new String[] { rawContactId, ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE };
+        ops.add(ContentProviderOperation.newUpdate(ContactsContract.Data.CONTENT_URI)
+                .withSelection(selection, selectionArgs)
+                .withValue(ContactsContract.CommonDataKinds.Organization.COMPANY, newOrganization)
+                .build());
+        try { getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops); } catch (Exception e) { Log.e(null, e.toString()); }
+        modal.setOrganization(newOrganization);
+        Log.e(null, "SUCCEED with " + newOrganization);
+    }
+
+    private void updateName (String newName) {
+        String rawContactId = String.valueOf(MainActivity.getRawContactId(context, contactId));
+        ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
+        String selection = ContactsContract.Data.RAW_CONTACT_ID + "=? AND " + ContactsContract.Data.MIMETYPE + "=?";
+        String[] nameParams = new String[]{rawContactId,
+                ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE};
+        ops.add(android.content.ContentProviderOperation.newUpdate(
+                        android.provider.ContactsContract.Data.CONTENT_URI)
+                .withSelection(selection, nameParams)
+                .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME,
+                        newName).build());
+        try { getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops); } catch (Exception e) { Log.e(null, e.toString()); }
+        modal.setUserName(newName);
+        Log.e(null, "SUCCEED with " + newName);
+    }
+
+    private void updateEmail (String newEmail) {
+        String rawContactId = String.valueOf(MainActivity.getRawContactId(context, contactId));
+        ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
+        String selection = ContactsContract.Data.RAW_CONTACT_ID + "=? AND " + ContactsContract.Data.MIMETYPE + "=?";
+        String[] selectionArgs = new String[] { rawContactId, String.valueOf(ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)};
+        ops.add(ContentProviderOperation.newUpdate(ContactsContract.Data.CONTENT_URI)
+                .withSelection(selection, selectionArgs)
+                .withValue(ContactsContract.CommonDataKinds.Email.DATA, newEmail)
+                .build());
+        try { getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops); } catch (Exception e) { Log.e(null, e.toString()); }
+        modal.setEmail(newEmail);
+        Log.e(null, "SUCCEED with " + newEmail);
+    }
+
+    private void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
